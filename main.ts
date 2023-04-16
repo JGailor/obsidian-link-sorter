017717e99837770a66aefee48c7bf92205eb319e
Jeremy Gailor <jgailor@gmail.com>
2023-04-16 10:27:36 -0700

    Initial implementation w/ settings for regex, target folder, and template

--- main.ts
+++ main.ts
@@ -1,81 +1,47 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, FileSystemAdapter, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface SmartLinkSettings {
+	pattern?: string;
+	folder?: string;
+	template?: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const SMART_LINK_DEFAULT_SETTINGS: SmartLinkSettings = {
+	pattern: null,
+	folder: null,
+	template: null
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class SmartLink extends Plugin {
+	settings: SmartLinkSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEvent(this.app.vault.on('create', (file: TAbstractFile) => {
+			if (this.settings.pattern && this.settings.folder) {
+				let regex: RegExp = RegExp(this.settings.pattern);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (regex.test(file.name) && file.parent.name != this.settings.folder) {
+					let target = this.app.vault.getRoot().path + this.settings.folder + "/" + file.name;
+					this.app.vault.adapter.exists(target).then((exists) => {
+						if (!exists) {
+							let target: string = "/" + this.settings.folder + "/" + file.name;
+							this.app.fileManager.renameFile(file, target).then(() => {
+								if (this.settings.template && this.app.vault.adapter.exists(this.settings.template)) {
+									this.app.vault.adapter.read(this.settings.template).then((contents) => {
+										this.app.vault.adapter.append(target, contents).then(() => {});
+									});
+								}
+							});
+						}					
+					});
 				}
 			}
-		});
+		}));
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SmartLinkSettingsTab(this.app, this));
 	}
 
 	onunload() {
@@ -83,7 +49,7 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, SMART_LINK_DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
@@ -91,26 +57,11 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class SmartLinkSettingsTab extends PluginSettingTab {
+	plugin: SmartLink;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SmartLink) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +71,39 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Smart Link Settings.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Filename pattern')
+			.setDesc('Use a RegEx here to match on a filename that will be automatically moved to the target folder on creation.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Regular Expression')
+				.setValue(this.plugin.settings.pattern)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.pattern = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('Target Folder')
+			.setDesc('When a new filename matches the above pattern, it will be moved into this folder.')
+			.addText(text => text
+				.setPlaceholder('Folder')
+				.setValue(this.plugin.settings.folder)
+				.onChange(async (value) => {
+					this.plugin.settings.folder = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Template')
+			.setDesc('If a file is matched, it can be created with this optional template.')
+			.addText(text => text
+				.setPlaceholder('Template <optional>')
+				.setValue(this.plugin.settings.template)
+				.onChange(async (value) => {
+					this.plugin.settings.template = value;
+					await this.plugin.saveSettings();
+				}));			
 	}
 }
