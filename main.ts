bd96fa86beb18a616f685f414b0c286a92b51bdc
Jeremy Gailor <jgailor@gmail.com>
2023-05-06 07:32:42 -0700

    Bunch of work to get the basic plugin working.  Installs, opens the SettingsTab, and supports adding, deleting, and editing settings

--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
-import { App, FileSystemAdapter, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { App, FileSystemAdapter, Modal, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 interface SmartLinkSettings {
+	settings: SmartLinkSetting[];
+}
+
+interface SmartLinkSetting {
+	name?: string;
 	pattern?: string;
 	folder?: string;
 	template?: string;
 }
 
 const SMART_LINK_DEFAULT_SETTINGS: SmartLinkSettings = {
-	pattern: null,
-	folder: null,
-	template: null
+	settings: [
+		{name: "People", pattern: "^@.*", folder: "People", template: "Templates/Person Template.md"},
+		{name: "Places", pattern: ".*@$", folder: "Places", template: null}
+	]
 }
 
 export default class SmartLink extends Plugin {
@@ -19,23 +25,26 @@ export default class SmartLink extends Plugin {
 		await this.loadSettings();
 
 		this.registerEvent(this.app.vault.on('create', (file: TAbstractFile) => {
-			if (this.settings.pattern && this.settings.folder) {
-				let regex: RegExp = RegExp(this.settings.pattern);
+			for (let idx = 0; idx < this.settings.settings.length; idx++) {
+				let setting = this.settings.settings[idx];
 
-				if (regex.test(file.name) && file.parent.name != this.settings.folder) {
-					let target = this.app.vault.getRoot().path + this.settings.folder + "/" + file.name;
+				let regex: RegExp = RegExp(setting.pattern);
+
+				if (regex.test(file.name) && file.parent.name != setting.folder) {
+					let target = this.app.vault.getRoot().path + "/" + setting.folder + "/" + file.name;
 					this.app.vault.adapter.exists(target).then((exists) => {
 						if (!exists) {
-							let target: string = "/" + this.settings.folder + "/" + file.name;
+							let target: string = "/" + setting.folder + "/" + file.name;
 							this.app.fileManager.renameFile(file, target).then(() => {
-								if (this.settings.template && this.app.vault.adapter.exists(this.settings.template)) {
-									this.app.vault.adapter.read(this.settings.template).then((contents) => {
+								if (setting.template && this.app.vault.adapter.exists(setting.template)) {
+									this.app.vault.adapter.read(setting.template).then((contents) => {
 										this.app.vault.adapter.append(target, contents).then(() => {});
 									});
 								}
 							});
 						}					
 					});
+					break;
 				}
 			}
 		}));
@@ -67,43 +76,119 @@ class SmartLinkSettingsTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		containerEl.createEl('h2', {text: 'Smart Link Settings.'});
+		for (let idx = 0; idx < this.plugin.settings.settings.length; idx++) {
+			let settingContainer = containerEl.createEl('div', {cls: 'setting-container'});
+			settingContainer.createEl('span', {text: this.plugin.settings.settings[idx].name, cls: 'setting-name'});
+			settingContainer.createEl('button', {text: 'edit', cls: 'settings-container-button'}).onClickEvent(() => {
+				new SmartLinkSettingsModal(this.app, this.plugin.settings.settings[idx], async (result) => {
+					if (result.name && result.pattern) {
+						this.plugin.settings.settings[idx] = result;
+						await this.plugin.saveSettings();
+						this.display();
+					}
+				}).open();				
+			});
+
+			settingContainer.createEl('button', {text: 'delete', cls: 'settings-container-button'}).onClickEvent(async (el, ev) => {
+				const removedSetting = this.plugin.settings.settings.splice(idx, 1);
+				await this.plugin.saveSettings();
+				this.display();
+			});
+
+			containerEl.createEl('br');
+		}
 
 		new Setting(containerEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText("New")
+					.setCta()
+					.onClick(() => {
+						new SmartLinkSettingsModal(this.app, {name: null, pattern: null, folder: null, template: null}, async (result) => {
+							if (result.name && result.pattern) {
+								this.plugin.settings.settings.push(result);
+								await this.plugin.saveSettings();
+								this.display();
+							}
+						}).open();
+					}));		
+	}
+}
+
+class SmartLinkSettingsModal extends Modal {
+	setting: SmartLinkSetting;
+  	onSubmit: (result: SmartLinkSetting) => void;
+
+  	constructor(app: App, setting: SmartLinkSetting, onSubmit: (result: SmartLinkSetting) => void) {
+    	super(app);
+    	this.setting = setting;
+    	this.onSubmit = onSubmit;
+  	}
+
+  	onOpen() {
+    	const { contentEl } = this;
+    	// containerEl.empty();
+    	contentEl.createEl('h2', {text: 'Smart Link Settings.'});
+
+		new Setting(contentEl)
+			.setName('Name')
+			.setDesc('For future reference')
+			.addText(text => text
+				.setPlaceholder('Name')
+				.setValue(this.setting.name)
+				.onChange(async (value) => {
+						this.setting.name = value;
+				}));
+
+		new Setting(contentEl)
 			.setName('Filename pattern')
 			.setDesc('Use a RegEx here to match on a filename that will be automatically moved to the target folder on creation.')
 			.addText(text => text
 				.setPlaceholder('Regular Expression')
-				.setValue(this.plugin.settings.pattern)
+				.setValue(this.setting.pattern)
 				.onChange(async (value) => {
-					this.plugin.settings.pattern = value;
-					await this.plugin.saveSettings();
+						this.setting.pattern = value;
 				}));
 
-		new Setting(containerEl)
+		new Setting(contentEl)
 			.setName('Target Folder')
 			.setDesc('When a new filename matches the above pattern, it will be moved into this folder.')
 			.addText(text => text
 				.setPlaceholder('Folder')
-				.setValue(this.plugin.settings.folder)
+				.setValue(this.setting.folder)
 				.onChange(async (value) => {
-					this.plugin.settings.folder = value;
-					await this.plugin.saveSettings();
+						this.setting.folder = value;
 				}));
 
-		new Setting(containerEl)
+		new Setting(contentEl)
 			.setName('Template')
 			.setDesc('If a file is matched, it can be created with this optional template.')
 			.addText(text => text
 				.setPlaceholder('Template <optional>')
-				.setValue(this.plugin.settings.template)
+				.setValue(this.setting.template)
 				.onChange(async (value) => {
-					this.plugin.settings.template = value;
-					await this.plugin.saveSettings();
-				}));			
-	}
+						this.setting.template = value;
+				}));
+
+
+		new Setting(contentEl)
+			.addButton((btn) => 
+				btn
+					.setButtonText("Submit")
+					.setCta()
+					.onClick(() => {
+						this.close();
+						this.onSubmit(this.setting);
+					}));
+  	}
+
+  	onClose() {
+    	let { contentEl } = this;
+    	contentEl.empty();
+  	}
 }
