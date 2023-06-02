0a9ccaf635a56d816f7761e324cda84dbd16cea8
Jeremy Gailor <jgailor@gmail.com>
2023-06-02 08:41:33 -0700

    Renamed plugin to Link Sorter

--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
 import { App, FileSystemAdapter, Modal, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-interface SmartLinkSettings {
-	settings: SmartLinkSetting[];
+interface LinkSorterSettings {
+	settings: LinkSorterSetting[];
 }
 
-interface SmartLinkSetting {
+interface LinkSorterSetting {
 	name?: string;
 	pattern?: string;
 	folder?: string;
 	template?: string;
 }
 
-const SMART_LINK_DEFAULT_SETTINGS: SmartLinkSettings = {
+const LINK_SORTER_DEFAULT_SETTINGS: LinkSorterSettings = {
 	settings: [
 		{name: "People", pattern: "^@.*", folder: "People", template: "Templates/Person Template.md"},
 		{name: "Places", pattern: ".*@$", folder: "Places", template: null}
 	]
 }
 
-export default class SmartLink extends Plugin {
-	settings: SmartLinkSettings;
+export default class LinkSorter extends Plugin {
+	settings: LinkSorter;
 
 	async onload() {
 		await this.loadSettings();
@@ -50,7 +50,7 @@ export default class SmartLink extends Plugin {
 		}));
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SmartLinkSettingsTab(this.app, this));
+		this.addSettingTab(new LinkSorterSettingsTab(this.app, this));
 	}
 
 	onunload() {
@@ -58,7 +58,7 @@ export default class SmartLink extends Plugin {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, SMART_LINK_DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, LINK_SORTER_DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
@@ -67,10 +67,10 @@ export default class SmartLink extends Plugin {
 }
 
 
-class SmartLinkSettingsTab extends PluginSettingTab {
-	plugin: SmartLink;
+class LinkSorterSettingsTab extends PluginSettingTab {
+	plugin: LinkSorter;
 
-	constructor(app: App, plugin: SmartLink) {
+	constructor(app: App, plugin: LinkSorter) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -80,12 +80,12 @@ class SmartLinkSettingsTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Smart Link Settings.'});
+		containerEl.createEl('h2', {text: 'Link Sorter Settings'});
 		for (let idx = 0; idx < this.plugin.settings.settings.length; idx++) {
 			let settingContainer = containerEl.createEl('div', {cls: 'setting-container'});
 			settingContainer.createEl('span', {text: this.plugin.settings.settings[idx].name, cls: 'setting-name'});
 			settingContainer.createEl('button', {text: 'edit', cls: 'settings-container-button'}).onClickEvent(() => {
-				new SmartLinkSettingsModal(this.app, this.plugin.settings.settings[idx], async (result) => {
+				new LinkSorterSettingsModal(this.app, this.plugin.settings.settings[idx], async (result) => {
 					if (result.name && result.pattern) {
 						this.plugin.settings.settings[idx] = result;
 						await this.plugin.saveSettings();
@@ -106,25 +106,25 @@ class SmartLinkSettingsTab extends PluginSettingTab {
 		new Setting(containerEl)
 			.addButton((btn) =>
 				btn
-					.setButtonText("New")
-					.setCta()
-					.onClick(() => {
-						new SmartLinkSettingsModal(this.app, {name: null, pattern: null, folder: null, template: null}, async (result) => {
-							if (result.name && result.pattern) {
-								this.plugin.settings.settings.push(result);
-								await this.plugin.saveSettings();
-								this.display();
-							}
-						}).open();
-					}));		
+				.setButtonText("New")
+				.setCta()
+				.onClick(() => {
+					new LinkSorterSettingsModal(this.app, {name: null, pattern: null, folder: null, template: null}, async (result) => {
+						if (result.name && result.pattern) {
+							this.plugin.settings.settings.push(result);
+							await this.plugin.saveSettings();
+							this.display();
+						}
+					}).open();
+				}));		
 	}
 }
 
-class SmartLinkSettingsModal extends Modal {
-	setting: SmartLinkSetting;
-  	onSubmit: (result: SmartLinkSetting) => void;
+class LinkSorterSettingsModal extends Modal {
+	setting: LinkSorterSetting;
+  	onSubmit: (result: LinkSorterSetting) => void;
 
-  	constructor(app: App, setting: SmartLinkSetting, onSubmit: (result: SmartLinkSetting) => void) {
+  	constructor(app: App, setting: LinkSorterSetting, onSubmit: (result: LinkSorterSetting) => void) {
     	super(app);
     	this.setting = setting;
     	this.onSubmit = onSubmit;
@@ -133,58 +133,60 @@ class SmartLinkSettingsModal extends Modal {
   	onOpen() {
     	const { contentEl } = this;
     	// containerEl.empty();
-    	contentEl.createEl('h2', {text: 'Smart Link Settings.'});
+    	contentEl.createEl('h2', {text: 'Link Sorter Settings.'});
 
 		new Setting(contentEl)
 			.setName('Name')
 			.setDesc('For future reference')
-			.addText(text => text
-				.setPlaceholder('Name')
-				.setValue(this.setting.name)
-				.onChange(async (value) => {
+			.addText((text) => {
+				text.setPlaceholder('Name')
+					.setValue(this.setting.name)
+					.onChange(async (value) => {
 						this.setting.name = value;
-				}));
+					})
+			});
 
 		new Setting(contentEl)
 			.setName('Filename pattern')
 			.setDesc('Use a RegEx here to match on a filename that will be automatically moved to the target folder on creation.')
-			.addText(text => text
-				.setPlaceholder('Regular Expression')
-				.setValue(this.setting.pattern)
-				.onChange(async (value) => {
+			.addText((text) => {
+				text.setPlaceholder('Regular Expression')
+					.setValue(this.setting.pattern)
+					.onChange(async (value) => {
 						this.setting.pattern = value;
-				}));
+					})
+			});
 
 		new Setting(contentEl)
 			.setName('Target Folder')
 			.setDesc('When a new filename matches the above pattern, it will be moved into this folder.')
-			.addText(text => text
-				.setPlaceholder('Folder')
-				.setValue(this.setting.folder)
-				.onChange(async (value) => {
+			.addText((text) => {
+				text.setPlaceholder('Folder')
+					.setValue(this.setting.folder)
+					.onChange(async (value) => {
 						this.setting.folder = value;
-				}));
+					});				
+			});
 
 		new Setting(contentEl)
 			.setName('Template')
 			.setDesc('If a file is matched, it can be created with this optional template.')
-			.addText(text => text
-				.setPlaceholder('Template <optional>')
-				.setValue(this.setting.template)
-				.onChange(async (value) => {
+			.addText((text) => {
+				text.setPlaceholder('Template <optional>')
+					.setValue(this.setting.template)
+					.onChange(async (value) => {
 						this.setting.template = value;
-				}));
+					})
+			});
 
-
-		new Setting(contentEl)
-			.addButton((btn) => 
-				btn
-					.setButtonText("Submit")
-					.setCta()
-					.onClick(() => {
-						this.close();
-						this.onSubmit(this.setting);
-					}));
+		let buttons = contentEl.createEl('div', {cls: 'buttons-container'});
+		buttons.createEl('button', {text: 'Cancel', cls: 'settings-modal-button'}).onClickEvent(() => {
+			this.close();
+		});
+		buttons.createEl('button', {text: 'Save', cls: 'settings-modal-button'}).onClickEvent(() => {
+			this.onSubmit(this.setting);
+			this.close();
+		});
   	}
 
   	onClose() {
